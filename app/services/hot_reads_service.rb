b84b8a7486c861015472c8bbcd66cc96c145383c
blackknight75 <devbox4714@gmail.com>
2017-06-08 01:10:36 -0600

    point service at endpoint

--- app/services/hot_reads_service.rb
+++ app/services/hot_reads_service.rb
@@ -1,7 +1,7 @@
 class HotReadsService < ActiveRecord::Base
 
   def self.get_hot_links
-    response = Faraday.get("https://boiling-woodland-88175.herokuapp.com/links")
+    response = Faraday.get("https://boiling-woodland-88175.herokuapp.com/api/v1links")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
