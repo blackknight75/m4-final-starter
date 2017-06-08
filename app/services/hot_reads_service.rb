a3d01b65291e36a88905bc512d1b21a84eec192d
blackknight75 <devbox4714@gmail.com>
2017-06-08 00:59:16 -0600

    fix localhost link on hot reads service

--- app/services/hot_reads_service.rb
+++ app/services/hot_reads_service.rb
@@ -1,7 +1,7 @@
 class HotReadsService < ActiveRecord::Base
 
   def self.get_hot_links
-    response = Faraday.get("http://localhost:2000/api/v1/links")
+    response = Faraday.get("https://boiling-woodland-88175.herokuapp.com/links")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
