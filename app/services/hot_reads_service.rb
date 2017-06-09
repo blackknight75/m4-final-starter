9d9e8efbecd03a1ca379f9debd3fb459ad2232d5
blackknight75 <devbox4714@gmail.com>
2017-06-09 10:21:46 -0600

    fix listener for new links appended to the page

--- app/services/hot_reads_service.rb
+++ app/services/hot_reads_service.rb
@@ -1,7 +1,8 @@
 class HotReadsService < ActiveRecord::Base
 
   def self.get_hot_links
-    response = Faraday.get("http://localhost:2000/api/v1/links")
+    response = Faraday.get("http://boiling-woodland-88175.herokuapp.com/api/v1/links")
+    # response = Faraday.get("http://localhost:2000/api/v1/links")
     JSON.parse(response.body, symbolize_names: true)
   end
 end
